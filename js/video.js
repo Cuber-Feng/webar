42b31cdac785e5e60ffd574c327cb0e5ee435d75
cuber-feng <2017feng35@gmail.com>
2025-06-12 10:16:46 +0800

    using external js

--- js/video.js
+++
@@ -1,7 +0,0 @@
-
-window.addEventListener("load", () => {
-    const video = document.querySelector("#my-video");
-    video.play().catch((e) => {
-        console.log("Autoplay failed:", e);
-    });
-});
